046369835849ef88781ef077c7f644fb5bfc3ec3
lokyaan <kotilokyaan@gmail.com>
2024-11-29 12:12:31 +0530

    Added new API endpoint /api/goodbye with updated Swagger documentation

--- routes/api.js
+++ routes/api.js
@@ -22,4 +22,27 @@ router.get('/hello', (req, res) => {
     res.json({ message: 'Hello, world!' });
 });
 
+
+/**
+ * @swagger
+ * /api/goodbye:
+ *   get:
+ *     summary: Returns a farewell message.
+ *     responses:
+ *       200:
+ *         description: A successful response.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Goodbye, world!"
+ */
+router.get('/goodbye', (req, res) => {
+    res.json({ message: 'Goodbye, world!' });
+});
+
+
 module.exports = router;
